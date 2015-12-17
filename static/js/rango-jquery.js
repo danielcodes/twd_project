744422293fd534b0eb4872a5e01e9d87f3cf555d
Daniel Chia <daniel_1118@hotmail.com>
2015-12-16 19:09:34 -0500

    added basic javascript nonsense

---
+++ static/js/rango-jquery.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+	// JQuery code to be added in here.
+
+	//add bootstrap classes
+	$("#about-btn").addClass('btn btn-primary')
+
+	//on click button event
+	$("#about-btn").click( function(event) {
+		msgstr = $("#msg").html()
+		msgstr = msgstr + "o"
+		$("#msg").html(msgstr)
+	});
+
+	//hover event
+	$("p").hover( 
+		//hover does mouse in and mouse out events
+		function() {
+		$(this).css('color', 'red');
+		},
+		function() {
+			$(this).css('color', 'blue');
+		});	
+
+});
+
